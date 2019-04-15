e9ef21992fcb9eec5b5902908b7ece48cab12211
zac <z.lewis@base2services.com>
2019-04-15 14:10:24 +0200

    Add the ability to specify an S3 bucket policy.

--- s3-cloudfront.cfndsl.rb
+++ s3-cloudfront.cfndsl.rb
@@ -12,13 +12,28 @@ CloudFormation do
       }
     ]
   }
-  
-  
-  S3_Bucket('Bucket') do 
+
+if defined? bucket_policy
+
+  bucket_policy.each do |sid, statement_config|
+    statement = {}
+    statement["Sid"] = sid
+    statement['Effect'] = statement_config.has_key?('effect') ? statement_config['effect'] : "Allow"
+    statement['Principal'] = statement_config.has_key?('principal') ? statement_config['principal'] : {AWS: FnSub("arn:aws:iam::${AWS::AccountId}:root")}
+    statement['Resource'] = statement_config.has_key?('resource') ? statement_config['resource'] : [FnJoin("",["arn:aws:s3:::", Ref('Bucket')]), FnJoin("",["arn:aws:s3:::", Ref('Bucket'), "/*"])]
+    statement['Action'] = statement_config.has_key?('actions') ? statement_config['actions'] : ["s3:*"]
+    statement['Condition'] = statement_config['conditions'] if statement_config.has_key?('conditions')
+    policy_document[:Statement] << statement
+  end
+
+end
+
+
+  S3_Bucket('Bucket') do
     BucketName FnSub(bucket_name)
   end
 
-  S3_BucketPolicy("BucketPolicy") do 
+  S3_BucketPolicy("BucketPolicy") do
     Bucket Ref('Bucket')
     PolicyDocument policy_document
   end
