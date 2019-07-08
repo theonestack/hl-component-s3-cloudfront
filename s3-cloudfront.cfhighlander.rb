4ddc2f2bf19c6e40e708cbbbf591b0405c0a16d5
zac <z.lewis@base2services.com>
2019-07-08 17:30:33 +1000

    Bump nested cloudfront version.

--- s3-cloudfront.cfhighlander.rb
+++ s3-cloudfront.cfhighlander.rb
@@ -8,7 +8,7 @@ CfhighlanderTemplate do
     ComponentParam 'DnsDomain', isGlobal: true
   end
 
-  Component name: cloudfront_component_name, template: 'git:https://github.com/rererecursive/hl-component-cloudfront.git#master', render: Inline do
+  Component name: cloudfront_component_name, template: 'cloudfront@0.4.0', render: Inline do
     parameter name: "s3bucketOriginDomainName", value: FnGetAtt('Bucket', 'DomainName')
   end
 
